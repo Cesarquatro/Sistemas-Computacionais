d9cdb748dbd14ef6bb54cd021e02921968bc4ddd
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2023-10-29 19:39:18 -0300

    bag_of_words

--- Trabalho/Individual_Chatbot/nltk_utils.py
+++ Trabalho/Individual_Chatbot/nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 nltk.download('punkt')
 
 # preprocessing techniques/utils
@@ -13,8 +14,19 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
-
+    '''
+    sentence =  ["hello", "how", "are", "you"]
+    all_words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag =   [ 0,     1,     0,   1,     0,     0,       0]
+    # put 1 if word in sentence match with all_words, else 0
+    '''
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32) # dtype=np.float32 - curso fit
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0 # 1.0 - curso fit 
+    return bag
+    
 '''
 a = "How long does shipping take?"
 print(a)
@@ -25,3 +37,8 @@ words = ["organize", "organizes", "organizing"]
 print(words)
 stemmed_words = [stem(w) for w in words]
 print(stemmed_words)'''
+
+sentence =  ["hello", "how", "are", "you"]
+all_words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag =   bag_of_words(sentence, all_words)
+print(bag)
